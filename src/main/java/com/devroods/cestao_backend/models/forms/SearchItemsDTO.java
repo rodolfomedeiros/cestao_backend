b949376da4e61e1efe29d0c4e15af561eb49b9ad
Rodolfo Medeiros <sd.rodolfo.medeiros@gmail.com>
2020-05-28 11:03:49 -0300

    fix: lombok

--- src/main/java/com/devroods/cestao_backend/models/forms/SearchItemsDTO.java
+++ src/main/java/com/devroods/cestao_backend/models/forms/SearchItemsDTO.java
@@ -2,36 +2,20 @@ package com.devroods.cestao_backend.models.forms;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
 public class SearchItemsDTO {
 
   private String query;
   private List<BusinessDTO> soldItemsByBusiness;
 
-  public SearchItemsDTO (){}
-
-  public SearchItemsDTO(String query, List<BusinessDTO> soldItemsByBusiness) {
-    this.query = query;
-    this.soldItemsByBusiness = soldItemsByBusiness;
-  }
-
-  public String getQuery() {
-    return query;
-  }
-
-  public void setQuery(String query) {
-    this.query = query;
-  }
-
-  public List<BusinessDTO> getSoldItemByBusiness() {
-    return soldItemsByBusiness;
-  }
-
-  public void setSoldItemsByBusiness(List<BusinessDTO> soldItemsByBusiness) {
-    this.soldItemsByBusiness = soldItemsByBusiness;
-  }
-
-  @Override
-  public String toString() {
-    return "SearchItemsDTO [query=" + query + ", soldItemsByBusiness=" + soldItemsByBusiness + "]";
-  }
 }
