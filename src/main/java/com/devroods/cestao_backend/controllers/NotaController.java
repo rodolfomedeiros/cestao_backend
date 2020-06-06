d3ed174e2cf27b74d1c24bf573502f71a2ebe574
Rodolfo Medeiros <sd.rodolfo.medeiros@gmail.com>
2020-06-06 17:02:59 -0300

    change: env variables

--- src/main/java/com/devroods/cestao_backend/controllers/NotaController.java
+++ src/main/java/com/devroods/cestao_backend/controllers/NotaController.java
@@ -3,7 +3,6 @@ package com.devroods.cestao_backend.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
